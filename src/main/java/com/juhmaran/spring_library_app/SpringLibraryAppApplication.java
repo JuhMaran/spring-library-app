1cce8b91656bf1d45507d18bcc572962f508de1f
JuhMaran <julianemaran@gmail.com>
2026-04-13 21:04:02 -0300

    initial project

---
+++ src/main/java/com/juhmaran/spring_library_app/SpringLibraryAppApplication.java
@@ -0,0 +1,13 @@
+package com.juhmaran.spring_library_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringLibraryAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringLibraryAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/juhmaran/spring_library_app/SpringLibraryAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.juhmaran.spring_library_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringLibraryAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
